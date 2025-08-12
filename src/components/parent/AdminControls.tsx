86b840245c37172854aa689b7ce45af35349fde9
jamesfjsk <paulombc123@gmail.com>
2025-08-12 11:22:10 -0300

    Updated AdminControls.tsx

--- src/components/parent/AdminControls.tsx
+++ src/components/parent/AdminControls.tsx
@@ -163,7 +163,7 @@ const AdminControls: React.FC = () => {
             <div className="w-full bg-purple-200 rounded-full h-2">
               <div 
                 className="bg-purple-600 h-2 rounded-full transition-all duration-500"
-                style={{ width: `${levelSystem.progressPercentage}%` }}              />
+                style={{ width: `${levelSystem.progressPercentage}%` }}
               />
             </div>
           </div>
