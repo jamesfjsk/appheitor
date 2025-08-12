70507d0d6184cb0fd2afd3b474c00a04a1a988bf
jamesfjsk <paulombc123@gmail.com>
2025-08-12 11:21:03 -0300

    Fix JSX syntax errors in FirebaseDoctor and AdminControls

--- src/components/common/FirebaseDoctor.tsx
+++ src/components/common/FirebaseDoctor.tsx
@@ -98,7 +98,8 @@ ${report.messages.join('\n')}
   };
 
   return (
-    <motion.div      initial={{ opacity: 0, y: 20 }}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       className="bg-white rounded-lg shadow-sm border border-gray-200 p-6"
     >
