006701113cc62c127676179e997d01dab72a5bb8
jamesfjsk <paulombc123@gmail.com>
2025-08-12 11:26:48 -0300

    Add default export to RewardManager.tsx

---
+++ src/components/parent/RewardManager.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RewardManager: React.FC = () => {
+  return (
+    <div>
+      {/* RewardManager component placeholder */}
+    </div>
+  );
+};
+
+export default RewardManager;
